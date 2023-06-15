5b68cecb4b5fd5a9c0da933cb8334d0aa530d652
maurilosantos <maurilosantoss1@gmail.com>
2023-06-15 12:01:23 -0300

    style: :art: deixa o componente de Parceiros responsivo

--- src/paginas/parceiros/Parceiros.tsx
+++ src/paginas/parceiros/Parceiros.tsx
@@ -4,34 +4,34 @@ import './Parceiros.css'; // Import the CSS file for styling
 const Parceiros = () => {
     const myArray = [
       {
-        foto: 'https://i.imgur.com/u2i29FU.png',
+        foto: 'https://images.unsplash.com/photo-1661956602153-23384936a1d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
         texto: 'É um espaço dedicado à conexão entre pessoas, comunidades e instituições para compartilhar demandas e necessidades públicas. Acredita-se na importância da participação cidadã e no poder do diálogo para construir uma sociedade mais justa e inclusiva. Através da plataforma, é possível compartilhar informações sobre infraestrutura, saúde, educação, segurança e muito mais, promovendo a transparência e a colaboração na resolução de problemas. Junte-se a eles e faça parte dessa rede engajada em tornar as demandas públicas em realidade.'
       },
       {
-        foto: 'https://i.imgur.com/ypoSIvr.png',
+        foto: 'https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
         texto: 'É comprometida em facilitar o acesso ao crédito personalizado e acessível para produtores rurais e empresas do agronegócio. Sua missão é fornecer serviços financeiros abrangentes, incluindo empréstimos, financiamentos, consultoria e seguros agrícolas, tudo adaptado às necessidades específicas de cada cliente. Com uma equipe de especialistas em agronegócio, está pronta para ajudar os produtores a prosperar e alcançar seus objetivos financeiros. Junte-se a eles e desfrute dos benefícios de uma parceria sólida e confiável para impulsionar o sucesso no campo.'
       },
       {
-        foto: 'https://i.imgur.com/2fj5c9Q.png',
+        foto: 'https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
         texto: 'Conecta produtores a insumos agropecuários de alta qualidade, impulsionando o agronegócio com soluções inovadoras e sustentáveis. Elimina intermediários tradicionais e oferece suporte técnico especializado para aumentar a eficiência e produtividade das atividades agrícolas. Trabalha com uma ampla gama de produtos, desde sementes e fertilizantes até defensivos agrícolas, sempre priorizando a qualidade e o sucesso dos clientes. Contar com essa empresa é impulsionar o negócio e enfrentar os desafios do campo com confiança.'
       },
       {
-        foto: 'https://i.imgur.com/mSyTK7P.png',
+        foto: 'https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80',
         texto: 'É referência na fabricação e fornecimento de equipamentos agrícolas de alta tecnologia. Sua missão é desenvolver equipamentos inovadores que aumentam a eficiência e produtividade no campo. Do projeto à fabricação, prioriza a qualidade, durabilidade e desempenho de seus produtos. Oferece uma ampla linha de tratores, colheitadeiras e implementos agrícolas especializados, todos projetados para atender às necessidades dos agricultores modernos. Com tecnologia avançada e suporte técnico especializado, está pronta para impulsionar a produção agrícola e levar os negócios a um novo patamar.'
       },
       {
-        foto: 'https://i.imgur.com/sFiHVWy.png',
+        foto: 'https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
         texto: 'Se dedica a oferecer cursos especializados em agroecologia, capacitando produtores e interessados em práticas agrícolas sustentáveis. Aborda temas como manejo orgânico, conservação de recursos naturais, biodiversidade e técnicas de cultivo sustentáveis. Seus cursos combinam teoria e prática, proporcionando conhecimentos valiosos e habilidades essenciais para uma agricultura mais sustentável.'
       },
       {
-        foto: 'https://i.imgur.com/kA7Hjim.png',
+        foto: 'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
         texto: 'A renomada Universidade Federal é reconhecida por sua excelência acadêmica em ciências agrárias. Oferece cursos de graduação, pós-graduação e pesquisa nas áreas da agricultura, agroecologia e afins. Comprometida com a formação de profissionais qualificados, a universidade proporciona um ambiente de aprendizado enriquecedor, com corpo docente experiente e infraestrutura de ponta. Além disso, promove a interação com a comunidade agrícola por meio de projetos de extensão, contribuindo para o desenvolvimento sustentável e a inovação no setor agropecuário. Junte-se a nós e faça parte de uma instituição comprometida com a excelência acadêmica e a transformação do agronegócio.'
       },
       ];
 
     
     const [linkDaImagemAtual, setLinkDaImagemAtual] = useState({
-      foto: "https://i.imgur.com/u2i29FU.png",
+      foto: "https://images.unsplash.com/photo-1661956602153-23384936a1d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
       texto: 'É um espaço dedicado à conexão entre pessoas, comunidades e instituições para compartilhar demandas e necessidades públicas. Acredita-se na importância da participação cidadã e no poder do diálogo para construir uma sociedade mais justa e inclusiva. Através da plataforma, é possível compartilhar informações sobre infraestrutura, saúde, educação, segurança e muito mais, promovendo a transparência e a colaboração na resolução de problemas. Junte-se a eles e faça parte dessa rede engajada em tornar as demandas públicas em realidade.'
     })
     
