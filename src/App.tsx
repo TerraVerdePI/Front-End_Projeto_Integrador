cae6f17788a7273caaee020e9d3ee36351a45808
maurilosantos <maurilosantoss1@gmail.com>
2023-06-18 18:28:51 -0300

    style: :art: responsividade barra de pesquisa
    Assim será possível se alinhas com a Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import LadingPage from './paginas/ladingPage/LadingPage';
 import PaginaProduto from './components/produtos/produtoPage/PaginaProduto';
 import Perfil from './paginas/perfil/Perfil';
 import Sobre from './paginas/sobre/Sobre';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Sidebar from './components/statics/sidebar/Sidebar';
 import { TokenState } from './store/tokens/tokensReducer';
@@ -28,53 +28,57 @@ import { TokenState } from './store/tokens/tokensReducer';
 function App() {
   return (
     < >
-      
+
       <Provider store={store}>
-      <ToastContainer />
-      <BrowserRouter>
-      <Navbar />
+        <ToastContainer />
+        <BrowserRouter>
+          <Navbar />
 
-        <div className="apptsx" style={{ minHeight: '100vh', width: 'calc(100vw-200px)', marginLeft: '40px' }}>
-        <Sidebar />
-        <Routes>
-        
-          <Route path='/' element={<LadingPage />} />
-
-          <Route path='/login' element={<Login />} />
-
-          <Route path='/home' element={<Home />} />
-
-          <Route path='/perfil' element={<Perfil />} />
-
-          <Route path='/sobre' element={<Sobre />} />
-
-          <Route path="/cadastro" element={<CadastroUsuario />} />
-
-          <Route path="/categorias" element={<ListaCategoria />} />
-
-          <Route path="/produtos" element={<ListaProduto />} />
-
-          <Route path="/formularioProduto" element={<CadastroProduto />} />
-
-          <Route path="/formularioProduto/:id" element={<CadastroProduto />} />
-
-          <Route path="/formularioCategoria" element={<CadastroCategoria />} />
-
-          <Route path="/formularioCategoria/:id" element={<CadastroCategoria />} />
-
-          <Route path="/deletarProduto/:id" element={<DeletarProduto />} />
-
-          <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
-
-          <Route path="/produtos/:id" element={<PaginaProduto />} />
           
-        </Routes>
-        
-        </div>
-      <Footer />
-        
-        
-      </BrowserRouter>
+            <div className="sidebar">
+              <Sidebar />
+            </div>
+            
+              <Routes>
+
+                <Route path='/' element={<LadingPage />} />
+
+                <Route path='/login' element={<Login />} />
+
+                <Route path='/home' element={<Home />} />
+
+                <Route path='/perfil' element={<Perfil />} />
+
+                <Route path='/sobre' element={<Sobre />} />
+
+                <Route path="/cadastro" element={<CadastroUsuario />} />
+
+                <Route path="/categorias" element={<ListaCategoria />} />
+
+                <Route path="/produtos" element={<ListaProduto />} />
+
+                <Route path="/formularioProduto" element={<CadastroProduto />} />
+
+                <Route path="/formularioProduto/:id" element={<CadastroProduto />} />
+
+                <Route path="/formularioCategoria" element={<CadastroCategoria />} />
+
+                <Route path="/formularioCategoria/:id" element={<CadastroCategoria />} />
+
+                <Route path="/deletarProduto/:id" element={<DeletarProduto />} />
+
+                <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+
+                <Route path="/produtos/:id" element={<PaginaProduto />} />
+
+              </Routes>
+            
+
+          
+          <Footer />
+
+
+        </BrowserRouter>
       </Provider>
     </>
   );
